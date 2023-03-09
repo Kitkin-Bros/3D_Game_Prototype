134f95376892b62728717972736d692be21a6f7d
Tikhomirov Ilya <110390530+IlyaT-Bearverce@users.noreply.github.com>
2023-03-09 13:12:49 +0300

    refactoring

--- Assets/Scripts/Services/Ui/AbstractView.cs
+++ Assets/Scripts/Services/Ui/AbstractView.cs
@@ -1,10 +1,19 @@
+using System;
 using UnityEngine;
 
 namespace Services.Ui {
     public abstract class AbstractView : MonoBehaviour {
-
-        [SerializeField]
+        
+        private const string ContentGoName = "Content";
         private GameObject _content;
+        
+        private void Start() {
+            _content = GameObject.Find(ContentGoName);
+            if (_content == null) {
+                throw new Exception($"\"Content\" element was not found in the hierarchy for {gameObject.name}");
+            }
+        }
+        
         public void Show() {
             _content.SetActive(true);
         }
